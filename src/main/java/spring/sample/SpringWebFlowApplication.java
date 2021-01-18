45de5cc6938a80c8a0a66877cbdd547a9220989d
Long <long.nguyen4@ntq-solution.com.vn>
2021-01-18 16:27:03 +0700

    first commit

---
+++ src/main/java/spring/sample/SpringWebFlowApplication.java
@@ -0,0 +1,13 @@
+package spring.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/sample/SpringWebFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
